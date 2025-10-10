d3cbb515bb2ffa2401f2e8c0e60577ff51fadb45
sammy-ulfh <sammy_ulfh@proton.me>
2025-10-10 10:44:35 -0600

    repo upgrade

--- Front-End/app/app/(tabs)/index.tsx
+++ Front-End/app/app/(tabs)/index.tsx
@@ -1,98 +1,75 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'TenorSans': require('../../assets/fonts/TenorSans-Regular.ttf'),
+    });
+    setFontsLoaded(true);
+  };
+
+  React.useEffect(() => {
+    loadFonts();
+  }, []);
+
+  return (
+    <ImageBackground
+      source={require('../../assets/main/background.png')}
+      style={styles.background}
+    >
+
+    <View style={[styles.totalWidth, styles.totalHeight, styles.container, styles.overlay]}>
+      <View style={[styles.totalWidth, styles.content]}>
+        <View style={[styles.totalWidth, styles.right, {height: '20%'}]}>
+          <View style={[{width: '80%', height: '100%'}]}>
+            <Text style={[styles.totalWidth, styles.totalHeight, styles.whiteText]}>
+              Donar a Más Bosque Manu, es donar para salvar una vida
+            </Text>
+          </View>
+        </View>
+        <View></View>
+        <View></View>
+      </View>
+    </View>
+
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  totalWidth: {
+    width: '100%',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  totalHeight: {
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  }, 
+  content: {
+    height: '50%',
+  },
+  whiteText: {
+    color: '#ffffff',
+    fontSize: 20,
+    fontFamily: 'TenorSans',
+
+  },
+  right: {
+    alignItems: 'flex-end',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject, // llena toda la superficie
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // negro con opacidad 50%
   },
 });
