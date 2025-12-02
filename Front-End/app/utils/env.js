bc2aa63511a2a694beb65d854f306ec1b8121e0c
AnaPau17 <anapaulanavh@gmail.com>
2025-12-02 17:11:22 -0600

    nada

---
+++ Front-End/app/utils/env.js
@@ -0,0 +1,6 @@
+// Generated from Front-End/1.env — local runtime shim for secrets (DO NOT COMMIT)
+export const SENTRY_DSN = "https://c43f2a5c2f199f2a976ddbdd9028f681@o4510354942656512.ingest.de.sentry.io/4510467222011984";
+
+export default {
+  SENTRY_DSN,
+};
