7b81dfb6889d92d44c6a957c98e40bc48f5eec35
AnaPau17 <anapaulanavh@gmail.com>
2025-12-02 17:23:08 -0600

    Remove env.js from repo

--- Front-End/app/utils/env.js
+++
@@ -1,6 +0,0 @@
-// (DO NOT COMMIT)
-export const SENTRY_DSN = "https://c43f2a5c2f199f2a976ddbdd9028f681@o4510354942656512.ingest.de.sentry.io/4510467222011984";
-
-export default {
-  SENTRY_DSN,
-};
