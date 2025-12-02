273a49e16ef1636785f80248e77c1c8edb19b710
AnaPau17 <anapaulanavh@gmail.com>
2025-12-02 17:13:51 -0600

    comentario

--- Front-End/app/utils/env.js
+++ Front-End/app/utils/env.js
@@ -1,4 +1,4 @@
-// Generated from Front-End/1.env — local runtime shim for secrets (DO NOT COMMIT)
+// (DO NOT COMMIT)
 export const SENTRY_DSN = "https://c43f2a5c2f199f2a976ddbdd9028f681@o4510354942656512.ingest.de.sentry.io/4510467222011984";
 
 export default {
